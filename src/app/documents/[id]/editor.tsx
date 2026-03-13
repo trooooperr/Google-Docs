f784c55ae47575124a2afc575701c39b91b74ac1
Alok Kumar Gupta <trooooperr@gmail.com>
2026-03-13 22:14:13 +0530

    tiptap inplementation

---
+++ src/app/documents/[id]/editor.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { useEditor, EditorContent } from '@tiptap/react';
+import StarterKit from '@tiptap/starter-kit';
+import { useEffect, useState } from 'react';
+
+export const Editor = () => {
+const [isMounted, setIsMounted] = useState(false);
+
+useEffect(() => {
+    setIsMounted(true); // mark component as mounted on client
+}, []);
+
+const editor = useEditor({
+    editorProps: {
+        attributes: {
+            style: "padding-left:56px; padding-right:56px;",
+            class: 'focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1504px] w-[816px] pt-10 pr-14 pb-10 cursor-text',
+        },
+    },
+    extensions: [StarterKit],
+    content: '<p>Hello World!</p>',
+    immediatelyRender: false, // important to prevent SSR rendering
+});
+
+if (!isMounted) return null; // render nothing on server
+
+return (
+<div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
+    <div className='min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0'>
+       <EditorContent editor={editor} />
+    </div>
+</div>
+);
+};
+
+export default Editor;
---
+++ src/app/documents/[id]/page.tsx
@@ -0,0 +1,13 @@
+// import { Editor } from "./editor";
+// interface PageProps {
+// params: Promise <{ id: string }>;
+// }
+// const DocumentsPage = async ({ params }: PageProps) => {
+//     const { id } = await params;
+//     return <div className="min-h-screen bg-[#FAFBFD]">
+//         <Editor />
+//     </div>
+//     ;
+// }
+
+// export default DocumentsPage
